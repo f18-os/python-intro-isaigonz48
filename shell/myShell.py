dc57fab2ad66555e8b2c50860597faeff416f45c
Icey <isaigonz48@gmail.com>
2018-09-09 16:28:37 -0600

    Progress on the shell

--- shell/myShell.py
+++ shell/myShell.py
@@ -3,26 +3,31 @@ import os, sys, time, re
 pid = os.getpid()
 
 os.write(1, ("Starting up shell\n").encode())
+exitNum = 0
+while True:
+    rc = os.fork()
 
-rc = os.fork()
+    if rc < 0:
+        os.write(2, ("Fork failed, returning %d\n" % rc).encode())
+        sys.exit()
 
-if rc < 0:
-    os.write(2, ("Fork failed, returning %d\n" % rc).encode())
-    sys.exit()
-
-elif rc == 0:
-    #os.write(1, ("%s : \n" % os.environ['PATH']))
-    
-    while True:
+    elif rc == 0:
+        #os.write(1, ("%s : \n" % os.environ['PATH']))while True:
         os.write(1, ("command prompt:").encode())
         userInput = os.read(0, 1024)
-        args = re.split(" ", userInput)
+        userInput = userInput[:-1]
+        
+        args = re.split(" ", userInput.decode())
+        #args = arg
         #args = {"blah", "pls"}
-        args[-1] = args[-1][:-1]
-
+        
         if args[-1] == "stop":
+            print (args[-1])
+            exitNum = 1
+            break
             sys.exit()
-
+        print (args[-1])
+        #print (args[1])
         #print (args[0])
         for dir in re.split(":", os.environ['PATH']):
             program = "%s/%s" % (dir, args[0])
@@ -30,13 +35,15 @@ elif rc == 0:
                 os.execve(program,args,os.environ)
             except FileNotFoundError:
                 pass
-
+            
         os.write(2, ("Error try again\n").encode())
         
         
-else:
-    #os.write(1, ("What happen"))
+    else:
+        #os.write(1, ("What happen"))
     
-    childPidCode = os.wait()
-    #time.sleep(2)
-    os.write(1, ("SEE YA\n").encode())
+        childPidCode = os.wait()
+        #time.sleep(2)
+        if(exitNum):
+            sys.exit()
+        os.write(1, ("SEE YA\n").encode())
