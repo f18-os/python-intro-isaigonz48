66b12c443f91f6f424347474ef863e422d888925
Icey <isaigonz48@gmail.com>
2018-09-10 13:51:02 -0600

    program now works with one > or >> input

--- shell/myShell.py
+++ shell/myShell.py
@@ -11,11 +11,27 @@ while True:
     userInput = userInput[:-1]
 
     if userInput.decode() == "stop":
-            #print (args[-1])
-            sys.exit()
-            #break
-            #sys.exit()
-        
+        #print (args[-1])
+        sys.exit()
+        #break
+        #sys.exit()
+
+    extOut = 0    
+    checkSymbols = re.split(" ", userInput.decode())
+    for arg in checkSymbols:
+        if arg == ">":
+            print ("1")
+            extOut = 1
+            temp = re.split(" > ", userInput.decode())
+            userInput = temp[0].encode()
+            outFile = temp[1]
+        elif arg == ">>":
+            print ("2")
+            extOut = 2
+            temp = re.split(" >> ", userInput.decode())
+            userInput = temp[0].encode()
+            outFile = temp[1]
+            
     rc = os.fork()
 
     if rc < 0:
@@ -24,8 +40,10 @@ while True:
 
     elif rc == 0:
         #os.write(1, ("%s : \n" % os.environ['PATH']))while True:
-        
-        args = re.split(" ", userInput.decode())
+        userInput = userInput.decode()
+        if userInput[-1] == ' ':
+            userInput = userInput[:-1]
+        args = re.split(" ", userInput)
         #args = arg
         #args = {"blah", "pls"}
         
@@ -33,16 +51,31 @@ while True:
         #print (args[1])
         #print (args[0])
 
-        for arg in args:
-            if arg == "|":
-
-            elif arg == ">":
-
-            elif arg == ">>":
-
+        if extOut == 2:
+            #print ("2")
+            with open(outFile, "r") as readFile:
+                curText = readFile.read()
+            readFile.close()
             
+            os.close(1)
+            sys.stdout = open(outFile, "w")
+            fd = sys.stdout.fileno()
+            os.set_inheritable(fd,True)
+            #os.write(1, ("Writing in here").encode())
+            os.write(1, (curText).encode())
 
-        
+        elif extOut == 1:
+            #print ("1")
+            os.close(1)
+            sys.stdout = open(outFile, "w")
+            fd = sys.stdout.fileno()
+            os.set_inheritable(fd,True)
+            #os.write(1, ("Writing in here").encode())
+
+        #print (args[0])
+        #print (args[1])
+        #print (args[2])
+        print (userInput)
         for dir in re.split(":", os.environ['PATH']):
             program = "%s/%s" % (dir, args[0])
             try:
