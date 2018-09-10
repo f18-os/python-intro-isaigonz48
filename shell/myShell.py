29524a122cc2cdf7af13c13852143462eb5ca6a8
Icey <isaigonz48@gmail.com>
2018-09-09 20:32:04 -0600

    Shell works for simple commands, starting to incorporate the rest

--- shell/myShell.py
+++ shell/myShell.py
@@ -5,6 +5,17 @@ pid = os.getpid()
 os.write(1, ("Starting up shell\n").encode())
 exitNum = 0
 while True:
+    os.write(1, ("command prompt:").encode())
+
+    userInput = os.read(0, 1024)
+    userInput = userInput[:-1]
+
+    if userInput.decode() == "stop":
+            #print (args[-1])
+            sys.exit()
+            #break
+            #sys.exit()
+        
     rc = os.fork()
 
     if rc < 0:
@@ -13,22 +24,25 @@ while True:
 
     elif rc == 0:
         #os.write(1, ("%s : \n" % os.environ['PATH']))while True:
-        os.write(1, ("command prompt:").encode())
-        userInput = os.read(0, 1024)
-        userInput = userInput[:-1]
         
         args = re.split(" ", userInput.decode())
         #args = arg
         #args = {"blah", "pls"}
         
-        if args[-1] == "stop":
-            print (args[-1])
-            exitNum = 1
-            break
-            sys.exit()
-        print (args[-1])
+        #print (args[-1])
         #print (args[1])
         #print (args[0])
+
+        for arg in args:
+            if arg == "|":
+
+            elif arg == ">":
+
+            elif arg == ">>":
+
+            
+
+        
         for dir in re.split(":", os.environ['PATH']):
             program = "%s/%s" % (dir, args[0])
             try:
@@ -44,6 +58,4 @@ while True:
     
         childPidCode = os.wait()
         #time.sleep(2)
-        if(exitNum):
-            sys.exit()
-        os.write(1, ("SEE YA\n").encode())
+        #os.write(1, ("SEE YA\n").encode())
